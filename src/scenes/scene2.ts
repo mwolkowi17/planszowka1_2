e02fc1f649f96ee548fabf3ff0c3aaea088b4a51
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2025-03-25 18:22:51 +0100

    elements on scene ready

--- src/scenes/scene2.ts
+++ src/scenes/scene2.ts
@@ -2,6 +2,11 @@ import { Scene } from "phaser";
 
 export class Scene2 extends Scene {
   przycisk_rzut_kostka: Phaser.GameObjects.Image;
+  postac1: Phaser.GameObjects.Image;
+  postac2: Phaser.GameObjects.Image;
+  kostka: Phaser.GameObjects.Image;
+  gracz1_name: Phaser.GameObjects.Text;
+  gracz2_name: Phaser.GameObjects.Text;
   constructor() {
     super({
       key: "GameScene2",
@@ -10,22 +15,43 @@ export class Scene2 extends Scene {
   create(): void {
     //adding assets to stage
     this.add.image(640, 360, "plansza_scena2").scale = 0.67;
+
     this.przycisk_rzut_kostka = this.add
       .image(1153, 435, "button_rzut")
       .setInteractive();
-
     this.przycisk_rzut_kostka.scale = 0.67;
-    const el = document.createElement("input");
-    // const stage = document.querySelector("div");
-    // const stage = document.getElementById("inner_cont");
-    // stage.appendChild(el);
-    // el.textContent = "Wpisz coś";
-    // el.style.height = "30px";
-    // el.style.width = "100px";
-    // el.style.position = "absolute";
-    // el.style.zIndex = "2";
-    // el.style.left = "45%";
-    // el.style.bottom = "30%";
+
+    this.postac1 = this.add.image(100, 130, "postac1");
+    this.postac1.scale = 0.7;
+    this.postac1.rotation = Math.PI * 1.2;
+
+    this.postac2 = this.add.image(110, 180, "postac2");
+    this.postac2.scale = 0.7;
+    this.postac2.rotation = Math.PI * 1.2;
+
+    this.kostka = this.add.image(1153, 534, "kostka1");
+    this.kostka.scale = 0.7;
+    let nameA = localStorage.getItem("player2");
+    if (localStorage.getItem("player2") === "") {
+      nameA = "Komputer";
+    }
+    this.gracz1_name = this.add.text(
+      1110,
+      220,
+      localStorage.getItem("player1"),
+      {
+        fontSize: "26px",
+        fontStyle: "bold",
+        color: "black",
+      }
+    );
+    this.gracz2_name = this.add.text(1110, 295, nameA, {
+      fontSize: "26px",
+      fontStyle: "bold",
+      color: "black",
+    });
+    // this.gracz1_name.style.fontSize = "50";
+    // this.gracz1_name.style.color = "black";
 
     //function for event mouse handlin
 
